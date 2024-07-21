8cd076cf603dc39eb18c841ccd824af9839e2ca1
alexgjeka <alexgjeka0@gmail.com>
2024-07-20 21:53:12 -0400

    crawlPage function

--- crawl.js
+++ crawl.js
@@ -1,5 +1,26 @@
 import { JSDOM } from 'jsdom'
 
+async function beginCrawl(baseURL){
+    try {
+        const response = await fetch(baseURL)
+
+        if (response.status > 399){
+            console.log(`error in fetch with status code: ${response.status} on page: ${baseURL}`)
+            return
+        }
+
+        const contentType = response.headers.get("content-type")
+        if (!contentType .includes("text/html")){
+            console.log(`non html response: ${contentType} on page: ${baseURL}`)
+            return
+        }
+
+        console.log(await response.text())
+    } catch (error) {
+        console.log(`error fetching page: ${error.message} on page ${baseURL}`)
+    }
+}
+
 export function normalizeURL(urlString){
     const urlObject = new URL(urlString);
     const hostPath =  `${urlObject.hostname}${urlObject.pathname}`
@@ -35,25 +56,48 @@ export function getURLsFromHTML(htmlBody, baseURL){
     return urls
 }
 
-export async function crawlPage(baseURL) {
+export async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+
+    const baseHostName = new URL(baseURL)
+    const currentHostName = new URL(currentURL)
+
+    if (baseHostName.hostname !== currentHostName.hostname){
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+
+    if (pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
    
     try {
         const response = await fetch(baseURL)
 
         if (response.status > 399){
             console.log(`error in fetch with status code: ${response.status} on page: ${baseURL}`)
-            return
+            return pages
         }
 
         const contentType = response.headers.get("content-type")
         if (!contentType .includes("text/html")){
             console.log(`non html response: ${contentType} on page: ${baseURL}`)
-            return
+            return pages
         }
 
-        console.log(await response.text())
+        const htmlBody = await response.text()
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for (const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
     } catch (error) {
         console.log(`error fetching page: ${error.message} on page ${baseURL}`)
     }
+    return pages
 
 }
